25f7ed2481adb51fa37ecc9b9597f5a7400ec5e5
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2021-06-12 09:33:11 +0545

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+const app = express();
+dotenv.config()
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+// https://www.mongodb.com/cloud/atlas
+
+// const CONNECTION_URL = 'mongodb+srv://username: password@cluster0.fktna.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running in port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);    
+
+
